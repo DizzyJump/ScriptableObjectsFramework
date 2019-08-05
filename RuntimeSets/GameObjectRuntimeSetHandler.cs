51e1a520462e4615ab6d2f5bc1d5e426ee742344
s_stolnikov <s_stolnikov@wargaming.net>
2019-08-05 18:07:21 +0300

    Добавил RuntimeSet-ы

---
+++ RuntimeSets/GameObjectRuntimeSetHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectRuntimeSetHandler : MonoBehaviour
+{
+    [SerializeField] private GameObjectsRuntimeSet[] sets;
+
+    void OnEnable()
+    {
+        for(int i=0; i<sets.Length; i++)
+            sets[i].Add(gameObject);
+    }
+
+    void OnDisable()
+    {
+        for(int i = 0; i < sets.Length; i++)
+            sets[i].Remove(gameObject);
+    }
+}
---
+++ RuntimeSets/RuntimeSet.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RuntimeSet<T> : EventObject where T : UnityEngine.Object 
+{
+    public enum Actions
+    {
+        None,
+        Add,
+        Remove
+    }
+
+    [SerializeField] List<T> items;
+
+    private T lastItemActionOn;
+    public T LastItemActionOn
+    {
+        get { return lastItemActionOn; }
+    }
+
+    public Actions LastTimeAction { get => lastTimeAction; }
+    public List<T> Items { get => items; }
+
+    private Actions lastTimeAction = Actions.None;
+
+    void OnEnable()
+    {
+        items = new List<T>();
+        ClearCache();
+    }
+
+    void OnDisable()
+    {
+        Items.Clear();
+        items = null;
+        ClearCache();
+    }
+
+    void ClearCache()
+    {
+        lastItemActionOn = null;
+        lastTimeAction = Actions.None;
+    }
+
+    public void Add(T item)
+    {
+        if (!Items.Contains(item))
+        {
+            items.Add(item);
+            lastItemActionOn = item;
+            lastTimeAction = Actions.Add;
+            Invoke();
+            ClearCache();
+        }
+            
+    }
+
+    public void Remove(T item)
+    {
+        if (Items.Contains(item))
+        {
+            items.Remove(item);
+            lastItemActionOn = item;
+            lastTimeAction = Actions.Remove;
+            Invoke();
+            ClearCache();
+        }
+    }
+
+    public bool isContain(T item)
+    {
+        return items.Contains(item);
+    }
+}
