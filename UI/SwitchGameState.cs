e092f70cdd15ee631196ae2cee1a573a3927b6fe
DizzyJump <mojpovelitel@gmail.com>
2019-09-26 23:44:39 +0300

    добавил скрипт смены состояния

---
+++ UI/SwitchGameState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchGameState : MonoBehaviour
+{
+    private GameStateChangeEvent gameState;
+    [SerializeField] private GameStates targetState;
+
+    public void Switch()
+    {
+        if(gameState==null)
+            gameState = GameStateChangeEvent.Import();
+        gameState.Value = targetState;
+    }
+}
