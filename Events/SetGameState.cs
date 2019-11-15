67e5ad667b45008249306c894458c06836c9a069
s_stolnikov <s_stolnikov@wargaming.net>
2019-11-15 11:58:24 +0300

    сериализация дикшинари

---
+++ Events/SetGameState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGameState : MonoBehaviour
+{
+    private GameStateChangeEvent gameState;
+
+    [SerializeField] private GameStates targetState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = GameStateChangeEvent.Import();
+        
+    }
+
+    public void SwitchState()
+    {
+        gameState.Value = targetState;
+    }
+}
